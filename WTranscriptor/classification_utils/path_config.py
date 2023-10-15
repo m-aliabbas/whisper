2cea188aea77ebf144df365b597c5ecd2c04af7b
Mohammad Ali Abbas <vynz420@gmail.com>
2023-10-15 15:30:31 +0500

    Silero Added

--- WTranscriptor/classification_utils/path_config.py
+++ WTranscriptor/classification_utils/path_config.py
@@ -1,4 +1,4 @@
-CLASSIFIER_MODULE_PATH = '/home/idrak/ali_own/farooq/'
+CLASSIFIER_MODULE_PATH = '/home/mohammadali/Desktop/waspak.co/farooq/'
 NER_PATH = 'med_ner_2'
 MODEL_NAME = 'model_medicare1'
 ENTITY_LIST = ['B-AGE','B-YOB','I-AGE']
